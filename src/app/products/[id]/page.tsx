30ba141f6c53eb8331429885435832fefa83a997
Vishwas Naveen <vishwas.naveen2580@gmail.com>
2026-03-30 21:37:50 +0530

    bug fix

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -7,8 +7,9 @@ async function getProduct(id: string): Promise<Product> {
   return res.json();
 }
 
-export default async function ProductDetailPage({ params }: { params: { id: string } }) {
-  const product = await getProduct(params.id);
+export default async function ProductDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const product = await getProduct(id);
 
   return (
     <main className="container mx-auto p-8">
